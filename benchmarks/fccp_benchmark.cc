5e0d1a7d82fe58bc09775c1a29022acd2e9fd5df
Mariano M. Chouza <mchouza@gmail.com>
2016-03-08 22:05:09 -0300

    Applying ClangFormat.

--- benchmarks/fccp_benchmark.cc
+++ benchmarks/fccp_benchmark.cc
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     }
     try
     {
-        io::CSVReader<6,io::trim_chars<>> r(argv[1]);
+        io::CSVReader<6, io::trim_chars<>> r(argv[1]);
         const char *p1, *p2, *p3, *p4, *p5, *p6;
         size_t field_count = 0;
         while (r.read_row(p1, p2, p3, p4, p5, p6))
