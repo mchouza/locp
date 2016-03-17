0601ecdbe72491b53760118285c11caf266af57e
Mariano M. Chouza <mchouza@gmail.com>
2016-03-17 00:28:34 -0300

    Preparing to handle escaped data.
    The delimiter function was converted to a static member of a policy class to facilitate the handling of escaped data in the future.

--- locp.h
+++ locp.h
@@ -33,17 +33,8 @@
 
 namespace locp
 {
-
 namespace
 {
-
-// Basic configuration with no quoting.
-template <uint8_t col_delimiter_, uint8_t row_delimiter_> struct no_quote_config
-{
-    static const uint8_t col_delimiter = col_delimiter_;
-    static const uint8_t row_delimiter = row_delimiter_;
-};
-
 // Does its best to fill a buffer.
 size_t read_all_(uint8_t *buffer, size_t buffer_size, int fd)
 {
@@ -62,75 +53,86 @@ size_t read_all_(uint8_t *buffer, size_t buffer_size, int fd)
     }
     return p - buffer;
 }
+}
 
-// Finds the delimiters in a buffer and records its positions.
-size_t find_delimiters_(uint16_t *delimiter_pos, size_t delimiter_pos_size,
-                        const uint8_t *buffer, size_t buffer_len,
-                        uint8_t col_delimiter, uint8_t row_delimiter)
+// Basic configuration with no quoting.
+template <uint8_t col_delimiter_, uint8_t row_delimiter_> struct no_quote_policy
 {
-    // input index
-    size_t i = 0;
-    // output index
-    size_t j = 0;
+    // Column delimiter constant.
+    static const uint8_t col_delimiter = col_delimiter_;
 
-    // masks
-    constexpr uint64_t mask_0x01 = ~(uint64_t)0 / 0xff;
-    constexpr uint64_t mask_0x7f = 0x7f * mask_0x01;
-    constexpr uint64_t mask_0x80 = 0x80 * mask_0x01;
-    uint64_t col_delimiter_mask = col_delimiter * mask_0x01;
-    uint64_t row_delimiter_mask = row_delimiter * mask_0x01;
+    // Row delimiter constant.
+    static const uint8_t row_delimiter = row_delimiter_;
 
-    // initial section processing loop
-    for (; (uintptr_t)&buffer[i] % 8 != 0; i++)
-        if (buffer[i] == col_delimiter || buffer[i] == row_delimiter)
-            delimiter_pos[j++] = i;
-
-    // processes 8 bytes at a time
-    for (; i + 7 < buffer_len; i += 8)
+    // Finds the delimiters in a buffer and records its positions.
+    static size_t find_delimiters(uint16_t *delimiter_pos,
+                                  size_t delimiter_pos_size,
+                                  const uint8_t *buffer, size_t buffer_len)
     {
-        // based on
-        // https://graphics.stanford.edu/~seander/bithacks.html#HasLessInWord
+        // input index
+        size_t i = 0;
+        // output index
+        size_t j = 0;
 
-        // accesses the buffer as a 64 bit integer
-        const uint64_t *b = (const uint64_t *)&buffer[i];
+        // masks
+        constexpr uint64_t mask_0x01 = ~(uint64_t)0 / 0xff;
+        constexpr uint64_t mask_0x7f = 0x7f * mask_0x01;
+        constexpr uint64_t mask_0x80 = 0x80 * mask_0x01;
+        uint64_t col_delimiter_mask = col_delimiter * mask_0x01;
+        uint64_t row_delimiter_mask = row_delimiter * mask_0x01;
 
-        // gets the matches as null bytes
-        uint64_t col_matches_as_0x00 = *b ^ col_delimiter_mask;
-        uint64_t row_matches_as_0x00 = *b ^ row_delimiter_mask;
+        // initial section processing loop
+        for (; (uintptr_t)&buffer[i] % 8 != 0; i++)
+            if (buffer[i] == col_delimiter || buffer[i] == row_delimiter)
+                delimiter_pos[j++] = i;
 
-        // leaves 0x80 in those positions
-        uint64_t col_matches_as_0x80 =
-            (mask_0x80 - (col_matches_as_0x00 & mask_0x7f)) &
-            ~col_matches_as_0x00 & mask_0x80;
-        uint64_t row_matches_as_0x80 =
-            (mask_0x80 - (row_matches_as_0x00 & mask_0x7f)) &
-            ~row_matches_as_0x00 & mask_0x80;
-        uint64_t matches_as_0x80 = col_matches_as_0x80 | row_matches_as_0x80;
-
-        // goes over the matches
-        while (matches_as_0x80 != 0)
+        // processes 8 bytes at a time
+        for (; i + 7 < buffer_len; i += 8)
         {
-            // stores the match position
-            delimiter_pos[j++] = i + __builtin_ctzll(matches_as_0x80) / 8;
+            // based on
+            // https://graphics.stanford.edu/~seander/bithacks.html#HasLessInWord
 
-            // clears it
-            matches_as_0x80 &= matches_as_0x80 - 1;
+            // accesses the buffer as a 64 bit integer
+            const uint64_t *b = (const uint64_t *)&buffer[i];
+
+            // gets the matches as null bytes
+            uint64_t col_matches_as_0x00 = *b ^ col_delimiter_mask;
+            uint64_t row_matches_as_0x00 = *b ^ row_delimiter_mask;
+
+            // leaves 0x80 in those positions
+            uint64_t col_matches_as_0x80 =
+                (mask_0x80 - (col_matches_as_0x00 & mask_0x7f)) &
+                ~col_matches_as_0x00 & mask_0x80;
+            uint64_t row_matches_as_0x80 =
+                (mask_0x80 - (row_matches_as_0x00 & mask_0x7f)) &
+                ~row_matches_as_0x00 & mask_0x80;
+            uint64_t matches_as_0x80 =
+                col_matches_as_0x80 | row_matches_as_0x80;
+
+            // goes over the matches
+            while (matches_as_0x80 != 0)
+            {
+                // stores the match position
+                delimiter_pos[j++] = i + __builtin_ctzll(matches_as_0x80) / 8;
+
+                // clears it
+                matches_as_0x80 &= matches_as_0x80 - 1;
+            }
         }
+
+        // final section processing loop
+        for (; i < buffer_len; i++)
+            if (buffer[i] == col_delimiter || buffer[i] == row_delimiter)
+                delimiter_pos[j++] = i;
+        delimiter_pos[j++] = buffer_len;
+
+        // returns the number of delimiters
+        return j;
     }
-
-    // final section processing loop
-    for (; i < buffer_len; i++)
-        if (buffer[i] == col_delimiter || buffer[i] == row_delimiter)
-            delimiter_pos[j++] = i;
-    delimiter_pos[j++] = buffer_len;
-
-    // returns the number of delimiters
-    return j;
-}
-}
+};
 
 // CSV parser class.
-template <typename config = no_quote_config<',', '\n'>> class CSVParser
+template <typename policy = no_quote_policy<',', '\n'>> class CSVParser
 {
     // Buffer.
     uint8_t buffer_[(1 << 16) - 1];
@@ -163,9 +165,8 @@ template <typename config = no_quote_config<',', '\n'>> class CSVParser
             read_all_(buffer_ + num_remaining_bytes,
                       sizeof(buffer_) - num_remaining_bytes, fd_);
         buffer_len_ = num_remaining_bytes + read_bytes;
-        delimiter_pos_len_ = find_delimiters_(
-            delimiter_pos_, sizeof(delimiter_pos_), buffer_, buffer_len_,
-            config::col_delimiter, config::row_delimiter);
+        delimiter_pos_len_ = policy::find_delimiters(
+            delimiter_pos_, sizeof(delimiter_pos_), buffer_, buffer_len_);
         field_num_ = 0;
         more_data_available_ = (buffer_len_ == sizeof(buffer_));
     }
@@ -203,7 +204,7 @@ template <typename config = no_quote_config<',', '\n'>> class CSVParser
         // delimiter, there are no more fields
         if (field_num_ + 1 == delimiter_pos_len_ && !more_data_available_ &&
             delimiter_pos_[field_num_ - 1] + 1 == delimiter_pos_[field_num_] &&
-            buffer_[delimiter_pos_[field_num_ - 1]] == config::row_delimiter)
+            buffer_[delimiter_pos_[field_num_ - 1]] == policy::row_delimiter)
             return false;
 
         // if the next field ends delimited by the end of the buffer, we refill
